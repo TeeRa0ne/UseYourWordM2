161a3b8feabdd6af560b6141ebbe00f24536d6f1
Alexis TALDIR <alexis.taldir@gmail.com>
2022-09-26 10:23:51 +0200

    Add config to display web page

---
+++ src/main/java/use_your_words/config/AppConfig.java
@@ -0,0 +1,10 @@
+package use_your_words.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("use_your_words")
+public class AppConfig {
+
+}
---
+++ src/main/java/use_your_words/controller/Vote.java
@@ -0,0 +1,14 @@
+package use_your_words.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vote")
+public class Vote {
+	@GetMapping
+	public String login() {
+		return "vote";
+	}
+} 
